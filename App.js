b67e09e9ab7313c055481954afdc9603250d02ff
Raju Kakarlapudi <rajukakarlapudi@wireless-10-147-33-60.public.utexas.edu>
2019-11-16 14:48:25 -0600

    Raju and Tejas's efforts on frontend

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import LoginScreen from './login.js'
+//import LoginScreen from './login.js'
+import EmojiScreen from './emoji.js'
 
 export default class App extends React.Component {
     render() {
         return (
-            <LoginScreen />
+            <EmojiScreen />
         );
     }
 }
--- emoji.js
+++ emoji.js
@@ -1,52 +1,76 @@
 import React, { Component } from "react";
-import * as React from 'react';
+// import * as React from 'react';
 import { WebView, View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
 import { Container, Item, Form, Input, Button, Label } from "native-base";
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faSmileBeam } from '@fortawesome/free-solid-svg-icons'
+import { faSmileBeam, faAlignCenter } from '@fortawesome/free-solid-svg-icons'
 import { faFrown } from '@fortawesome/free-solid-svg-icons'
 import { faAngry } from '@fortawesome/free-solid-svg-icons'
+import { faSadTear } from '@fortawesome/free-solid-svg-icons'
+import { faKissWinkHeart } from '@fortawesome/free-solid-svg-icons'
+import { faMeh } from '@fortawesome/free-solid-svg-icons'
 import { TouchableOpacity } from 'react-native';
+import LoginScreen from './login.js'
 import { faGrimace } from '@fortawesome/free-solid-svg-icons'
 import * as Font from 'expo-font';
 import FontAwesome from "@expo/vector-icons/build/FontAwesome";
-import StarRating from 'react-native-star-rating';
-import AppLoading from "expo/build/launch/AppLoading";
+// import StarRating from 'react-native-star-rating';
+// import AppLoading from "expo/build/launch/AppLoading";
 import { TextInput } from 'react-native';
 
 import styles from "./style";
-import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView} from 'react-native';
-import { Button } from 'react-native-elements';
+//import {Keyboard, Text, View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView} from 'react-native';
+//import { Button } from 'react-native-elements';
 
 const appId = "1047121222092614"
 
-export default class LoginScreen extends Component {
+export default class Emoji extends Component {
 
     render() {
         return (
-            <View>
-            <View>
-                <FontAwesomeIcon icon={faSmileBeam} color="yellow" size={50} onPress={() => this.props.navigation.navigate('Happy1')} />
-                <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontWeight: "bold" }}>Happy</Text>
+            <View style={{backgroundColor: 'blue', flex: 1,
+            alignItems: "center",
+            justifyContent: "center"}}>
+               <ImageBackground source={require('./background.jpg')} style={{width: 1000, height: 1000,flex: 1}}>
+                <View style= {{flexDirection: 'row', marginLeft: 327, marginTop: 400}}>
+            <View style={{marginHorizontal: 20}}>
+                <FontAwesomeIcon icon={faSmileBeam} color="white" size={75} onPress={() => this.props.navigation.navigate('./login.js')} />
+                <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontWeight: "bold",marginRight: 50 }}></Text>
             </View>
 
-            <View>
-            <FontAwesomeIcon icon={faFrown} color="yellow" size={50} onPress={() => this.props.navigation.navigate('Sad1')} />
-        <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontWeight: "bold" }}>Sad</Text>
+            <View style={{marginHorizontal: 20}}>
+            <FontAwesomeIcon icon={faSadTear} color="white" size={75} onPress={() => this.props.navigation.navigate('./login.js')} />
+        <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontWeight: "bold",marginRight: 50}}></Text>
         </View>
 
-        <View>
-            <FontAwesomeIcon icon={faAngry} color="yellow" size={50} />
-            <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontWeight: "bold" }}>Mad</Text>
+        <View style={{marginHorizontal: 20}}>
+            <FontAwesomeIcon icon={faAngry} color="#ff9785" size={75} />
+            <Text style={{ textAlign: 'center', marginTop: 5, color: '#ff7961', fontWeight: "bold",marginRight: 50}}></Text>
         </View>
+ 
+        </View>
+        </ImageBackground>
 
-        <View >
-        <FontAwesomeIcon icon={faGrimace} color="yellow" size={50} />
-        <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontWeight: "bold" }}>Scared</Text>
+
+  <View style= {{flexDirection: 'row', marginVertical: 220}}>
+        <View style={{marginHorizontal: 20}}>
+        <FontAwesomeIcon icon={faGrimace} color="white" size={75} />
+        <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontWeight: "bold",marginRight: 50}}></Text>
         </View>
+        <View style={{marginHorizontal: 20}}>
+        <FontAwesomeIcon icon={faKissWinkHeart} color="white" size={75} />
+        <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontWeight: "bold",marginRight: 50}}></Text>
+        </View>
+        <View style={{marginHorizontal: 20}}>
+        <FontAwesomeIcon icon={faMeh} color="white" size={75} />
+        <Text style={{ textAlign: 'center', marginTop: 5, color: 'white', fontWeight: "bold",marginRight: 50}}></Text>
+        </View>
+        
             </View>
+            </View>
+       
         );
     }
 
